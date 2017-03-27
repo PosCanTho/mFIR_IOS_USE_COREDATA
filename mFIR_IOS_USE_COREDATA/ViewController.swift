9df67c7e67b7dd142db005076a9aedc61ca02016
Pham Van Thien <pvthiendeveloper@hackintoshs-mac.local>
2017-03-27 11:02:45 +0700

    Initial Commit

---
+++ mFIR_IOS_USE_COREDATA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mFIR_IOS_USE_COREDATA
+//
+//  Created by Pham Van Thien on 3/27/17.
+//  Copyright © 2017 poscantho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
