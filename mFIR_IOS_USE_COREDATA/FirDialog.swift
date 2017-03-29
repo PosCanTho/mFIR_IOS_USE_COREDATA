a9d439b936c9574eaff86e557c25db181369d364
Tống Thành Vinh <jack@Jack.local>
2017-03-29 10:25:21 +0700

    services hoan thien

--- mFIR_IOS_USE_COREDATA/FirDialog.swift
+++ mFIR_IOS_USE_COREDATA/FirDialog.swift
@@ -17,16 +17,79 @@ class FirDialog{
         
     }
     
-    static func showErrorDownload(viewController: UIViewController, urlError: URLError){
-        if(urlError.code == URLError.timedOut){
-            
-            let mes = "Hết thời gian kết nối.\nMã lỗi: \(urlError.errorCode)"
-            self.show( viewController: viewController, title: "Connection", mes: mes, buttonName: "Close")
-        }
-        if(urlError.code == URLError.notConnectedToInternet){
-            
-            let mes = "Không có kết nối internet. Vui lòng kiểm tra lại kết nối internet!\nMã lỗi: \(urlError.errorCode)"
-            self.show( viewController: viewController, title: "Connection", mes: mes, buttonName: "Close")
-        }
+}
+
+class ProgressingViewController: UIViewController {
+    
+    private let activityView = ActivityView()
+    
+    
+    
+    init(message: String) {
+        super.init(nibName: nil, bundle: nil)
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overFullScreen
+        activityView.messageLabel.text = message
+        view = activityView
+    }
+    
+    func setMessage(mes: String){
+        activityView.messageLabel.text = mes
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private class ActivityView: UIView {
+    
+    let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+    let boundingBoxView = UIView(frame: CGRect.zero)
+    //    let ProgressView = UIProgressView(frame: CGRect.zero)
+    let messageLabel = UILabel(frame: CGRect.zero)
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        
+        backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        
+        boundingBoxView.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        boundingBoxView.layer.cornerRadius = 12.0
+        
+        activityIndicatorView.startAnimating()
+        
+        messageLabel.font = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
+        messageLabel.textColor = UIColor.white
+        messageLabel.textAlignment = .center
+        messageLabel.shadowColor = UIColor.black
+        messageLabel.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        messageLabel.numberOfLines = 0
+        
+        addSubview(boundingBoxView)
+        addSubview(activityIndicatorView)
+        addSubview(messageLabel)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        boundingBoxView.frame.size.width = 160.0
+        boundingBoxView.frame.size.height = 160.0
+        boundingBoxView.frame.origin.x = ceil((bounds.width / 2.0) - (boundingBoxView.frame.width / 2.0))
+        boundingBoxView.frame.origin.y = ceil((bounds.height / 2.0) - (boundingBoxView.frame.height / 2.0))
+        
+        activityIndicatorView.frame.origin.x = ceil((bounds.width / 2.0) - (activityIndicatorView.frame.width / 2.0))
+        activityIndicatorView.frame.origin.y = ceil((bounds.height / 2.0) - (activityIndicatorView.frame.height / 2.0))
+        
+        let messageLabelSize = messageLabel.sizeThatFits(CGSize(width: 160.0 - 20.0 * 2.0, height: CGFloat.greatestFiniteMagnitude))
+        messageLabel.frame.size.width = messageLabelSize.width
+        messageLabel.frame.size.height = messageLabelSize.height
+        messageLabel.frame.origin.x = ceil((bounds.width / 2.0) - (messageLabel.frame.width / 2.0))
+        messageLabel.frame.origin.y = ceil(activityIndicatorView.frame.origin.y + activityIndicatorView.frame.size.height + ((boundingBoxView.frame.height - activityIndicatorView.frame.height) / 4.0) - (messageLabel.frame.height / 2.0))
     }
 }
