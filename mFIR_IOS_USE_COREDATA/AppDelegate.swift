891493108990f1d5947d2562a2bb4d4f6a99a79b
Baslor <baslor@Hackintoshs-Mac.local>
2017-03-30 09:00:38 +0700

    edasd

--- mFIR_IOS_USE_COREDATA/AppDelegate.swift
+++ mFIR_IOS_USE_COREDATA/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().tintColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
         return true
     }
 
