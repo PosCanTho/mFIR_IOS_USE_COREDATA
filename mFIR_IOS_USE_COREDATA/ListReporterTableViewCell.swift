a30cfc920185ccadfc4654787e9333a334e2e747
Pham Van Thien <pvthiendeveloper@Hackintoshs-Mac.local>
2017-03-30 09:01:54 +0700

    oe

---
+++ mFIR_IOS_USE_COREDATA/ListReporterTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListReporterTableViewCell.swift
+//  mFIR_IOS_USE_COREDATA
+//
+//  Created by Pham Van Thien on 3/29/17.
+//  Copyright © 2017 poscantho. All rights reserved.
+//
+
+import UIKit
+
+class ListReporterTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
