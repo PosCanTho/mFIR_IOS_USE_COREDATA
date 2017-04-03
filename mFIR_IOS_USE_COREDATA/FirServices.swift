be6dbf29cdfad7acb8bd6c6e8c2ad9d9b6e3aac4
Tong Thanh Vinh <vjnhgom@gmail.com>
2017-04-03 08:04:03 +0700

    fix 03/04

--- mFIR_IOS_USE_COREDATA/FirServices.swift
+++ mFIR_IOS_USE_COREDATA/FirServices.swift
@@ -1124,8 +1124,6 @@ class FirServices{
         
         let dataBase64 = imageData.base64EncodedString(options: .lineLength64Characters)
         
-        print(dataBase64)
-        
         var post = ""
         post += "<?xml version='1.0' encoding='utf-8'?>"
         post += "<soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'>"
@@ -1182,6 +1180,8 @@ class FirServices{
             print(Constants.ERROR.FILE_IMAGE_NAME)
             return
         }
+        
+        let fileName = fileName + ".jpg"
         
         taskFileImage(url: Constants.URL.API_DOWNLOAD_FILE_IMAGE, post: post) { (result) in
             
