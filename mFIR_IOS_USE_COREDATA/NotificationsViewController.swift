88d45fc0436508b8d450a7459d92da06fb64dec4
Pham Van Huong <HuongPOS@Hackintoshs-Mac.local>
2017-04-04 16:17:28 +0700

    custom 4/4/2017

--- mFIR_IOS_USE_COREDATA/NotificationsViewController.swift
+++ mFIR_IOS_USE_COREDATA/NotificationsViewController.swift
@@ -25,28 +25,14 @@ class NotificationsViewController: UITableViewController {
     
     
     @IBAction func btnFromDate(_ sender: UIButton) {
-
-        let currentDate = Date()
-        var dateComponents = DateComponents()
-        dateComponents.month = -3
-        let threeMonthAgo = Calendar.current.date(byAdding: dateComponents, to: currentDate)
-
-        
-        DatePickerDialog().show("From date", doneButtonTitle: "Done", cancelButtonTitle: "Cancel", minimumDate: threeMonthAgo, maximumDate: currentDate, datePickerMode: .date) { (date) in
-            if let dt = date {
-                print("----------ngay: \(dt)")
-                
-                let formatFromdate = DateFormatter()
-                formatFromdate.dateFormat = "dd/MM/yyy"
-                let datef = formatFromdate.string(from: dt)
-                
-                print("----------ngay: \(datef)")
-                self.lblFromdate.setTitle("\(datef)", for: .normal)
-            }
-        }
-
+        sortFromDate()
     }
     @IBAction func btnSortFromDate(_ sender: UIButton) {
+        sortFromDate()
+    }
+    
+
+    func sortFromDate(){
         let currentDate = Date()
         var dateComponents = DateComponents()
         dateComponents.month = -3
@@ -69,27 +55,12 @@ class NotificationsViewController: UITableViewController {
     }
     
     @IBAction func btnThruDate(_ sender: UIButton) {
-        let currentDate = Date()
-        var dateComponents = DateComponents()
-        dateComponents.month = -3
-        let threeMonthAgo = Calendar.current.date(byAdding: dateComponents, to: currentDate)
-        
-        
-        DatePickerDialog().show("Thru date", doneButtonTitle: "Done", cancelButtonTitle: "Cancel", minimumDate: threeMonthAgo, maximumDate: currentDate, datePickerMode: .date) { (date) in
-            if let dt = date {
-                print("----------ngay: \(dt)")
-                
-                let formatFromdate = DateFormatter()
-                formatFromdate.dateFormat = "dd/MM/yyy"
-                let datef = formatFromdate.string(from: dt)
-                
-                print("----------ngay: \(datef)")
-                self.lblThruDate.setTitle("\(datef)", for: .normal)
-            }
-        }
-
+        sortThruDate()
     }
     @IBAction func btnSortThruDate(_ sender: UIButton) {
+        sortThruDate()
+    }
+    func sortThruDate(){
         let currentDate = Date()
         var dateComponents = DateComponents()
         dateComponents.month = -3
@@ -112,26 +83,46 @@ class NotificationsViewController: UITableViewController {
     }
     
     @IBAction func btnStatus(_ sender: UIButton) {
-        print("status")
+        sortStatus()
     }
     @IBAction func btnSortStatus(_ sender: UIButton) {
+        sortStatus()
+    }
+    func sortStatus(){
+        let alert = UIAlertController(title: "Facility Issue Status", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        let oneAction = UIAlertAction(title: strChuaXuLy1, style: UIAlertActionStyle.default, handler: nil)
+        let twoAction = UIAlertAction(title: strDangXuLy1, style: UIAlertActionStyle.default, handler: nil)
+        let threeAction = UIAlertAction(title: strDaXuLy1, style: UIAlertActionStyle.default, handler: nil)
+        let fourAction = UIAlertAction(title: strKhongXuLyDuoc1, style: UIAlertActionStyle.default, handler: nil)
+        
+        alert.addAction(oneAction)
+        alert.addAction(twoAction)
+        alert.addAction(threeAction)
+        alert.addAction(fourAction)
+        
+        present(alert, animated: true, completion: nil)
+        
         print("status")
+
     }
     
   
     
-    
+    var facilityIssueStatus: [IssueStatus] = []
     var facilityIssue: [Issue] = []
     var issueNew: [Issue] = []
-    //var reversedIssueNew: [Issue] = []
-    //var issueSortByFacId:[Issue] = [] //list issue sort theo facilityId
-    let strNoStatus = "No status"
-    let strChuaXuLy = "Chưa xử lý"
-    let strDangXuLy = "Đang Xử lý"
-    let strDaXuLy = "đã xử lý"
-    let strKhongXuLyDuoc = "Không Xử Lý được"
+   
+    
+    var strChuaXuLy  = ""
+    var strDangXuLy = ""
+    var strDaXuLy = ""
+    var strKhongXuLyDuoc = ""
+    var strChuaXuLy1  = ""
+    var strDangXuLy1 = ""
+    var strDaXuLy1 = ""
+    var strKhongXuLyDuoc1 = ""
 
-    //123
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.title = "Notifications"
@@ -145,16 +136,15 @@ class NotificationsViewController: UITableViewController {
                 return
             }
             print(data!.userName)
-            
         }
-        
-        // Do any additional setup after loading the view.
         getData()
+        
     }
     
     func getData(){
         let issue = FirServices (self)
-        //Lấy ngày giờ hệ thống
+        let facilityIssueStatus = FirServices(self)
+        
         //let date = Date()
         let format = DateFormatter()
         format.dateFormat = "yyyy-MM-dd hh:mm:ss"
@@ -163,8 +153,27 @@ class NotificationsViewController: UITableViewController {
         //let strFromDate = "1994-11-11 00:00:00"
         let strFacilityIssueId = "0"
         
-        //print("ngay he thong ne: \(datef)")
         
+        //Lấy facility issue status
+         facilityIssueStatus.getIssueStatus { (issueStatusData) in
+            if issueStatusData != nil {
+                self.facilityIssueStatus = issueStatusData!
+                
+                self.strChuaXuLy = (self.facilityIssueStatus[0].typeNameName).lowercased()
+                self.strDangXuLy = (self.facilityIssueStatus[1].typeNameName).lowercased()
+                self.strKhongXuLyDuoc = (self.facilityIssueStatus[2].typeNameName).lowercased()
+                self.strDaXuLy = (self.facilityIssueStatus[3].typeNameName).lowercased()
+                
+                self.strChuaXuLy1 = self.facilityIssueStatus[0].typeNameName
+                self.strDangXuLy1 = self.facilityIssueStatus[1].typeNameName
+                self.strKhongXuLyDuoc1 = self.facilityIssueStatus[2].typeNameName
+                self.strDaXuLy1 = self.facilityIssueStatus[3].typeNameName
+
+                
+            }
+        }
+        
+        //Lấy facility issue
         issue.getIssue(facilityIssueId: strFacilityIssueId, fromDate: "", thruDate: "") { (dataIssue) in
             if dataIssue != nil {
                 DispatchQueue.main.async {
@@ -172,8 +181,6 @@ class NotificationsViewController: UITableViewController {
                     self.facilityIssue = dataIssue! //dữ liệu nguyễn mấu từ services
                     
                     ///Nhóm FacilityIssue theo facilityId (phòng)
-                    
-                   
                     for index in 0 ..< self.facilityIssue.count{
                       
                         let issue1: [Issue] = [self.facilityIssue[index]] //lấy đối tượng trong array
@@ -183,27 +190,23 @@ class NotificationsViewController: UITableViewController {
                         var isExist: Bool = false
                         
                         for index1 in 0 ..< self.issueNew.count {
-                            let strFacilityId1 = self.facilityIssue[index1].facilityId
+                            let strFacilityId1 = self.issueNew[index1].facilityId
                             if strFacilityId?.caseInsensitiveCompare(strFacilityId1!) == .orderedSame {
                                 isExist = true
-                                                           }
-                        
+                            }
+
                         }
                         if !isExist {
                             self.issueNew.append(contentsOf: issue1)
                             //print("jsfsssrff \(self.issueNew)")
-                            
                         }
-                        
-                        
                     }
-                    //Dao nguoc mang issuenew
+                    //Dao nguoc mang issueNew
                     self.issueNew.reverse()
                     
                     self.tableView.reloadData()
                     print("Lấy được rồi nhé!-------- OK")
                 }
-                
             }
         }
     }
@@ -244,40 +247,45 @@ class NotificationsViewController: UITableViewController {
         
         //kiem tra facilityIssueStatus
         
-        //let strFacilityIssueStatus = issue.facilityIssueStatus!
-        let strFacilityIssueStatus = issue.facilityIssueStatus!
+        let strFacilityIssueStatus = (issue.facilityIssueStatus!).lowercased()
+       
 
-        if(strFacilityIssueStatus.isEmpty){
+        //
+        if(strFacilityIssueStatus == ""){
             
-            cell.lblStatusRoom?.text = strNoStatus
+            cell.lblStatusRoom?.text = "Chưa Xử Lý"
             cell.lblStatusRoom.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-            cell.btnStatusRoom.setTitle("N", for: .normal)
+            cell.btnStatusRoom.setTitle("C", for: .normal)
             cell.btnStatusRoom.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
             
         }
         else
         {
             cell.lblStatusRoom?.text = issue.facilityIssueStatus
+            //Chưa xử lý
             if(strFacilityIssueStatus == strChuaXuLy){
-                cell.lblStatusRoom?.text = strFacilityIssueStatus
+                cell.lblStatusRoom?.text = strChuaXuLy1
                 cell.lblStatusRoom.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
                 cell.btnStatusRoom.setTitle("C", for: .normal)
                 cell.btnStatusRoom.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
             }
+            //Đang xử lý
             if(strFacilityIssueStatus == strDangXuLy){
-                cell.lblStatusRoom?.text = strFacilityIssueStatus
+                cell.lblStatusRoom?.text = strDangXuLy1
                 cell.lblStatusRoom.textColor = #colorLiteral(red: 0.983184278, green: 0.6165204644, blue: 0.01970458217, alpha: 1)
                 cell.btnStatusRoom.setTitle("Đ", for: .normal)
                 cell.btnStatusRoom.backgroundColor = #colorLiteral(red: 0.983184278, green: 0.6165204644, blue: 0.01970458217, alpha: 1)
             }
+            //Không xử lý được
             if(strFacilityIssueStatus == strKhongXuLyDuoc){
-                cell.lblStatusRoom?.text = strFacilityIssueStatus
+                cell.lblStatusRoom?.text = strKhongXuLyDuoc1
                 cell.lblStatusRoom.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
                 cell.btnStatusRoom.setTitle("K", for: .normal)
                 cell.btnStatusRoom.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 0.9313730736)
             }
+            //Đã xử lý
             if(strFacilityIssueStatus == strDaXuLy){
-                cell.lblStatusRoom?.text = strFacilityIssueStatus
+                cell.lblStatusRoom?.text = strDaXuLy1
                 cell.lblStatusRoom.textColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
                 cell.btnStatusRoom.setTitle("Đ", for: .normal)
                 cell.btnStatusRoom.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
